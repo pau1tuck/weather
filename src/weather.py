84f349b90f0d1a14493e48ae9eebf57291305156
Paul Tuck <paul.tuck@oxon.org>
2023-02-09 06:41:27 +0700

    "2023-02-09_06:41_ICT+7"

---
+++ src/weather.py
@@ -0,0 +1,18 @@
+from decouple import config
+import requests
+import json
+
+api_key = config("API_KEY")
+
+url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Mae%20Chan?unitGroup=metric&include=current&key=ASRQL55QSXYKL63Y2DCEMFUC3&contentType=json"
+
+response = requests.get(url)
+text = response.text
+
+weather_data = json.loads(text)
+print(weather_data["currentConditions"])
+
+current_conditions = weather_data["currentConditions"]
+temperature = current_conditions["temp"]
+
+print(f" |  {temperature:.0f}°C  |")
