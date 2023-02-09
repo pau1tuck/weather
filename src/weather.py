8382a1459155b3155c91e890bd4dc3e7179d257c
Paul Tuck <paul.tuck@oxon.org>
2023-02-09 16:30:30 +0700

    "2023-02-09_16:30_ICT+7"

--- src/weather.py
+++ src/weather.py
@@ -1,18 +1,26 @@
+import json
+from datetime import datetime
 from decouple import config
 import requests
-import json
 
-api_key = config("API_KEY")
+API_KEY = config("API_KEY")
+LOCATION = config("LOCATION")
 
-url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Mae%20Chan?unitGroup=metric&include=current&key={api_key}&contentType=json"
+url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{LOCATION}?unitGroup=metric&include=current&key={API_KEY}&contentType=json"
 
-response = requests.get(url)
-text = response.text
-
-weather_data = json.loads(text)
-print(weather_data["currentConditions"])
+location_response = requests.get("http://ipinfo.io/json", timeout=10).text
+weather_response = requests.get(url, timeout=10)
+weather_raw = weather_response.text
+location_data = json.loads(location_response)
+weather_data = json.loads(weather_raw)
 
 current_conditions = weather_data["currentConditions"]
 temperature = current_conditions["temp"]
 
-print(f" |  {temperature:.0f}°C  |")
+date = datetime.now()
+timezone = location_data["timezone"]
+
+print(str(date.strftime("%a, %b %-d, %Y, %-I:%M %p")) + " (" + str(timezone) + ")")
+
+print("Current Conditions:")
+print(f"Temperature: {temperature:.0f}°C")
